6a925fd9067b015522aea279b2daa6f2d1775b7a
CloakyLoki <101637404+CloakyLoki@users.noreply.github.com>
2023-02-06 15:50:51 +0300

    Jacoco fix

---
+++ common/src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
+}
