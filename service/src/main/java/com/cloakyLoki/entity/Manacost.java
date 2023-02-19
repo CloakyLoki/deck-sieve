67d1cca308afb5abb9f44374922628a39a48fcc1
CloakyLoki <101637404+CloakyLoki@users.noreply.github.com>
2023-02-19 23:23:46 +0300

    Manacost class change NOT NULL add to script

--- service/src/main/java/com/cloakyLoki/entity/Manacost.java
+++ service/src/main/java/com/cloakyLoki/entity/Manacost.java
@@ -1,15 +1,11 @@
 package com.cloakyLoki.entity;
 
-import com.cloakyLoki.entity.enumerated.ColorIndicator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -22,13 +18,11 @@ import javax.persistence.Table;
 public class Manacost {
 
     @Id
-    @Column(name = "card_id")
-    private Integer cardId;
-
-    @Column(name = "color_name")
-    @Enumerated(EnumType.STRING)
-    private ColorIndicator colorName;
-
-    @Column(name = "color_qty")
-    private Integer colorQty;
+    private Integer id;
+    private Integer red;
+    private Integer black;
+    private Integer green;
+    private Integer blue;
+    private Integer white;
+    private Integer common;
 }
--- service/src/main/resources/DB-init.sql
+++ service/src/main/resources/DB-init.sql
@@ -2,56 +2,60 @@
 CREATE TABLE IF NOT EXISTS cards
 (
     id                       SERIAL PRIMARY KEY,
-    name                     VARCHAR(128),
+    name                     VARCHAR(128) NOT NULL UNIQUE ,
     mana_value               INT,
-    types                    VARCHAR(256),
+    types                    VARCHAR(256) NOT NULL ,
     subtypes                 VARCHAR(256),
     supertypes               VARCHAR(256),
-    rarity                   VARCHAR(64),
-    text                     VARCHAR(512),
+    rarity                   VARCHAR(64) NOT NULL ,
+    text                     VARCHAR(512) NOT NULL ,
     flavor_text              VARCHAR(512),
-    keywords                 VARCHAR(256),
+    keywords                 VARCHAR(256) NOT NULL ,
     power                    INT,
     toughness                INT,
     purchase_url             VARCHAR(256),
     scryfall_illustration_id VARCHAR(256),
-    is_banned                BOOLEAN
+    is_banned                BOOLEAN NOT NULL
 );
 
 -- Colors need to cast a card
 CREATE TABLE IF NOT EXISTS cards_manacost_qty
 (
-    card_id    INT REFERENCES cards (id),
-    color_name VARCHAR(16),
-    color_qty  INT
+    card_id INT REFERENCES cards (id),
+    red     INT,
+    black   INT,
+    green   INT,
+    blue    INT,
+    white   INT,
+    common  INT
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
     id        SERIAL PRIMARY KEY,
-    nickname  VARCHAR(128),
-    password  VARCHAR(128),
-    role      VARCHAR(64),
-    is_active BOOLEAN
+    nickname  VARCHAR(128) NOT NULL UNIQUE ,
+    password  VARCHAR(128) NOT NULL ,
+    role      VARCHAR(64) NOT NULL ,
+    is_active BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS decks
 (
     id      SERIAL PRIMARY KEY,
-    name    VARCHAR(128),
-    user_id INT REFERENCES users (id)
+    name    VARCHAR(128) NOT NULL ,
+    user_id INT REFERENCES users (id) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS favourites_decks
 (
-    user_id INT REFERENCES users (id),
-    deck_id INT REFERENCES decks (id),
+    user_id INT REFERENCES users (id) NOT NULL ,
+    deck_id INT REFERENCES decks (id) NOT NULL ,
     PRIMARY KEY (user_id, deck_id)
 );
 
 CREATE TABLE IF NOT EXISTS deck_cards
 (
-    deck_id INT REFERENCES decks (id),
-    card_id INT REFERENCES cards (id),
+    deck_id INT REFERENCES decks (id) NOT NULL ,
+    card_id INT REFERENCES cards (id) NOT NULL ,
     PRIMARY KEY (card_id, deck_id)
 );
