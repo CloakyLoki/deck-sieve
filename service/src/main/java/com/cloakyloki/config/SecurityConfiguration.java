a46680904976edb25b97bb549e4c1a19bb63d857
CloakyLoki <101637404+CloakyLoki@users.noreply.github.com>
2023-04-24 00:00:49 +0300

    login, logout fixes card search pagination

--- service/src/main/java/com/cloakyloki/config/SecurityConfiguration.java
+++ service/src/main/java/com/cloakyloki/config/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static com.cloakyloki.entity.enumerated.Role.ADMIN;
-import static com.cloakyloki.entity.enumerated.Role.USER;
 
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -17,18 +16,17 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf().disable()
                 .authorizeHttpRequests(
                         urlConfig -> urlConfig
-                                .antMatchers("/login", "/index", "/cards/**", "/users/registration", "/swagger-ui/**", "/v3/api-docs/**", "/js/**/", "/css/**/", "/img/**/").permitAll()
-                                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                                .antMatchers("/login", "/index", "/cards/**", "/users/registration").permitAll()
+                                .antMatchers("/swagger-ui/**", "/v3/api-docs/**", "/js/**/", "/css/**/", "/img/**/").permitAll()
+                                .antMatchers(HttpMethod.GET, "/users/**").hasAuthority(ADMIN.getAuthority())
                                 .antMatchers("/admin/**", "/users/{\\d+}/delete", "/cards/{\\d+}/delete").hasAuthority(ADMIN.getAuthority())
-                                .antMatchers("/cards/**").hasAnyAuthority(ADMIN.getAuthority(), USER.getAuthority())
                                 .anyRequest().authenticated()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login"))
+                        .logoutSuccessUrl("/index"))
                 .formLogin(login -> login
                         .loginPage("/login")
                         .defaultSuccessUrl("/index"));
--- service/src/main/java/com/cloakyloki/dto/PageResponse.java
+++ service/src/main/java/com/cloakyloki/dto/PageResponse.java
@@ -3,7 +3,10 @@ package com.cloakyloki.dto;
 import lombok.Value;
 import org.springframework.data.domain.Page;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Value
 public class PageResponse<T> {
@@ -11,8 +14,19 @@ public class PageResponse<T> {
     List<T> content;
     Metadata metadata;
 
+
     public static <T> PageResponse<T> of(Page<T> page) {
-        var metadata = new Metadata(page.getNumber(), page.getSize(), page.getTotalPages());
+        List<Integer> pageNumbers;
+        int PAGE_SIZE = 20;
+        int totalPages = page.getTotalPages();
+        if (totalPages > 0) {
+            pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+        } else {
+            pageNumbers = Collections.emptyList();
+        }
+        var metadata = new Metadata(page.getNumber(), PAGE_SIZE, page.getTotalPages(), pageNumbers);
         return new PageResponse<>(page.getContent(), metadata);
     }
 
@@ -21,5 +35,6 @@ public class PageResponse<T> {
         int page;
         int size;
         long totalPages;
+        List<Integer> pageNumbers;
     }
 }
