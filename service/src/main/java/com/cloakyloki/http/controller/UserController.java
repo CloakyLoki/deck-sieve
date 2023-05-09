648e473757c954b2cea864133c4d19ae093f97e3
CloakyLoki <101637404+CloakyLoki@users.noreply.github.com>
2023-05-10 01:36:24 +0300

    Exception Handler

--- service/src/main/java/com/cloakyloki/http/controller/UserController.java
+++ service/src/main/java/com/cloakyloki/http/controller/UserController.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.groups.Default;
 import java.util.List;
@@ -52,14 +50,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String create(@ModelAttribute @Validated({Default.class, CreateAction.class}) UserCreateUpdateDto user,
-                         BindingResult bindingResult,
-                         RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("user", user);
-            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
-            return "redirect:/users/registration";
-        }
+    public String create(@ModelAttribute @Validated({Default.class, CreateAction.class}) UserCreateUpdateDto user) {
         userService.create(user);
         return "redirect:/login";
     }
